84b8e98d04ed5981fed20d494551fcb2221493a3
edilson gomes de lima <edskateicevida@gmail.com>
2024-09-11 03:32:19 -0300

    atualização

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,13 @@
-import logo from "./logo.svg";
-import "./App.css";
+import "./App.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> Hello world
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+import FirstComponent from "../../Components/FirstComponent";
+
+function App(){
+  return <div className="App">
+    <h1>Fundamento do React</h1>
+    <FirstComponent/>
+  </div>
+};
+
 
 export default App;
